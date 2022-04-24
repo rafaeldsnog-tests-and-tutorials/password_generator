66b6409b477b7a488299fc89ac94b0d6959e5525
Rafa <65832023+Rafaeldsnog@users.noreply.github.com>
2022-04-24 20:48:22 -0300

    Updated

--- password_generator.py
+++ password_generator.py
@@ -9,11 +9,6 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
-password_length = nr_letters + nr_symbols + nr_numbers
-
 len_letters = len(letters)
 len_numbers = len(numbers)
 len_symbols = len(symbols)
